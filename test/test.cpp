9612e9b8a9cf0cd342efb73a07c2dd55d0c73d25
Levin Gerdes <levin.gerdes@esa.int>
2019-02-07 10:55:36 +0100

    Prepare test directory

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE HazardDetectorLibrary
+#include <boost/test/unit_test.hpp>
+#include "hazard_detector/HazardDetector.hpp"
+
+using namespace hazard_detector;
+
+BOOST_AUTO_TEST_CASE(this_should_always_succeed)
+{
+    BOOST_CHECK(true);
+}
